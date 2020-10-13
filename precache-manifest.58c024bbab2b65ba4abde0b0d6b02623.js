2fcc57e40fcf57c79129bd1dd1de2a0ea95ea084
abhinaw <abhinaw706@gmail.com>
2020-10-13 14:09:37 +0530

    Updates

---
+++ precache-manifest.58c024bbab2b65ba4abde0b0d6b02623.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "895cceaf46721aef2702117c77d592a4",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "1c3d75aa670f8678cf94",
+    "url": "/react-deploy/static/css/2.614afd85.chunk.css"
+  },
+  {
+    "revision": "eb3c5b063487a3adc018",
+    "url": "/react-deploy/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "1c3d75aa670f8678cf94",
+    "url": "/react-deploy/static/js/2.6cf5ce46.chunk.js"
+  },
+  {
+    "revision": "eb3c5b063487a3adc018",
+    "url": "/react-deploy/static/js/main.22d69a26.chunk.js"
+  },
+  {
+    "revision": "4bbd4cc65473aff00d33",
+    "url": "/react-deploy/static/js/runtime~main.82fe89cb.js"
+  }
+]);
