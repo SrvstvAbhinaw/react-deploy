f1ee94a783023456da1e6daee9d46e6fdbd642c2
abhinaw <abhinaw706@gmail.com>
2020-10-13 14:02:16 +0530

    Deploy my app on github pages

---
+++ src/App.js
@@ -0,0 +1,51 @@
+
+
+
+
+import React, {Component} from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router,Route,Link} from "react-router-dom";
+
+import CreateTodo from "./components/create-todo.component";
+import EditTodo from "./components/edit-todo.commponent";
+import TodoList from "./components/todos-list.component";
+
+import logo from './logo.png';
+
+class App extends Component{
+  render (){
+    return (
+      <Router>
+        <div className="container">
+        
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="https://codingthesmartway.com">
+        <img src={logo} width="30" height="30" alt="codingthesmartway"/>
+        </a>
+        <Link to="/" className="navbar-brand">MERN Stack TODO App</Link>
+        
+        <div>
+        <ul className="navbar-nav mr-auto">
+          {/* Todos */}
+          <li className="navbar-item">
+            <Link to="/" className="nav-link">Todos</Link>
+          </li>
+          {/* Create Todos */}
+          <li className="navbar-item">
+            <Link to="/create" className="nav-link">Create Todos</Link>
+          </li>
+        </ul>
+        </div>
+        </nav>
+
+        <Route path="/" exact component={TodoList}/>
+        <Route path="/edit/:id"  component={EditTodo}/>
+        <Route path="/create"  component={CreateTodo}/>
+        </div>
+        
+      </Router>
+    );
+  }
+}
+
+export default App;
